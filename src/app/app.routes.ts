1a8135fe9722a49653cf2f3f1ee13c1362f5062d
isra brahmi <Esra2brahmi>
2025-05-05 11:52:00 +0100

    Save local changes to app.routes.ts before pulling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -113,10 +113,8 @@ export const routes: Routes = [
   {
     path: 'auth',
     loadComponent: () => import('./auth-component/auth-component.component').then( m => m.AuthComponentComponent)
-  },  {
-    path: 'calendar-tool',
-    loadComponent: () => import('./calendar-tool/calendar-tool.page').then( m => m.CalendarToolPage)
   },
+  
 
 
  
